6b9148181575bfac60d8debc6e2b8cacdd352bb6
Supanat Wiboonpanit <supanataom1710@gmail.com>
2025-04-29 11:04:34 +0700

    first commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ message.message }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'; // Correct import statement
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      message: 'Hello, World!'
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000')
+      .then(response => {
+        console.log(response.data);
+        this.message = response.data;
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* ให้เต็มความสูงของ viewport */
+}
+</style>
